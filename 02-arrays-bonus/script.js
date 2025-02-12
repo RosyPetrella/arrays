74923556e8aea5bd235c955e9ad09b3917252479
RosyPetrella <petrellamr@gmail.com>
2025-02-12 15:38:36 +0100

    edit task 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -7,8 +7,17 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter((teacher) => teacher.length >= 5);
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  const thisTeacher = teachers[i];
+  if (thisTeacher.length >= 5) {
+    longNames.push(thisTeacher);
+  }
+}
 console.log(longNames);
+
+// const longNames = teachers.filter((teacher) => teacher.length >= 5);
+// console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(5, 1);
